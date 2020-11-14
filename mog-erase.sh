0695dd974fa3040ba6e144a2df33391b5985b021
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-11-14 15:28:41 +0100

    Create mog-erase.sh
    Erase old Mogwai Node

---
+++ mog-erase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+cd ~
+echo "Stopping MOG Node"
+sleep 1
+systemctl stop MOGWAI
+sleep 2
+systemctl disable MOGWAI
+sleep 2
+echo "Erasing Files"
+rm -r mogwai*
+sleep 2
+rm /usr/local/bin/mogwai*
+sleep 2
+rm /etc/systemd/system/MOGWAI.service
+sleep 2 
+echo " Mogwai node Erased"
+rm mog-erase.sh
