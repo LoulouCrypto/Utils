38badc7977ac397c37f6287bc3558b54ad4884d1
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-11-14 12:57:13 +0100

    Create pirl-erase.sh

---
+++ pirl-erase.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+cd ~
+echo "disable node"
+sudo systemctl stop marlin
+sleep 5
+sudo systemctl stop pirl
+sleep 5
+sudo systemctl disable pirl
+sudo systemctl disable marlin
+sleep 2
+echo "Removing folders"
+sudo rm -r .pirl .marlin mn_installer
+sleep 2
+sudo rm -r /root/.pirl
+sleep 2
+sudo rm -r /root/marlin
+sleep 2
+sudo rm /usr/bin/pirl
+sleep 2
+sudo rm /usr/bin/marlin
+sleep 2
+sudo rm -r /etc/pirl*
+sleep 2
+sudo rm -f /usr/local/bin/pirl-premium-core 
+sleep 2
+sudo rm -f /etc/systemd/system/pirlnode.service 
+sleep 2
+echo "Reloading SystemD"
+sudo systemctl daemon-reload
