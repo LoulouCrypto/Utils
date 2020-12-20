76d3006af7604f2cc2ca8d0dd6c5675e75fa72b4
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2020-12-20 13:03:14 +0100

    Update addswap.sh

--- addswap.sh
+++ addswap.sh
@@ -3,7 +3,7 @@
 # https://www.louloucrypto.fr
  echo -e "${RED}How many running Go of swap do you want to add on this server ? ( add M after the number)"
  read -e SWAP
-sudo SWAPFILE=$(mktemp)
+SWAPFILE=$(mktemp)
 sudo dd if=/dev/zero of=$SWAPFILE bs=1024 count=$SWAP
 sudo chmod 600 $SWAPFILE
 sudo mkswap $SWAPFILE
