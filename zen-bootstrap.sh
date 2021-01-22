9622e60cd7248a0c684360e1139e20cd1f0af8a4
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-01-22 15:38:35 +0100

    Create zen-bootstrap.sh

---
+++ zen-bootstrap.sh
@@ -0,0 +1,15 @@
+  
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+cd ~/.zen
+sudo systemctl stop zend
+sleep 10
+sudo killall zend
+sleep 2
+rm -r blocks chainstate
+wget https://bootstraps.ultimatenodes.io/horizen/masternode/horizen_masternode_blockchain_2021-01-22_10-00-02_UTC.tar.gz
+sleep 2
+tar xzf horizen_masternode_blockchain_2021-01-22_10-00-02_UTC.tar.gz
+sleep 2
+rm horizen_masternode_blockchain_2021-01-22_10-00-02_UTC.tar.gz
