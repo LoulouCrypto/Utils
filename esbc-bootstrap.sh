a7087a07e70eb074b1d4d1a2788b42b222bdcee3
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-04-05 12:27:12 +0200

    Update esbc-bootstrap.sh

--- esbc-bootstrap.sh
+++ esbc-bootstrap.sh
@@ -9,7 +9,7 @@ esbcoin-cli stop
 sleep 2
 rm -r blocks chainstate database how-to-use.txt macOS_update.sh db.log debug.log peers.dat
 sleep 2 
-wget https://files.esbc.pro/bootstrap.zip
+wget https://zang.ovh/bootstraps/esbc/bootstrap.zip
 sleep 1 
 unzip bootstrap.zip
 sleep 1
