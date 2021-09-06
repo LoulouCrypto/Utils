29f9d54f0b918aca240d7976dbcea85aadc2ba7a
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-09-06 21:56:24 +0200

    Update bootstrap-nrg.sh

--- bootstrap-nrg.sh
+++ bootstrap-nrg.sh
@@ -7,6 +7,8 @@ sudo systemctl stop energi3
 sleep 1
 energi3 import-preimages preimages.rlp
 sleep 1
+energi3 removedb
+sleep 1
 cd
 sleep 1
 curl -s https://s3-us-west-2.amazonaws.com/download.energi.software/releases/chaindata/mainnet/gen3-chaindata.tar.gz | tar xvz
