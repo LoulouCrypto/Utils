d2c2d52f72c06fc2092dc7d4be40a9e7747165be
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-09-06 21:53:12 +0200

    Create bootstrap-nrg.sh

---
+++ bootstrap-nrg.sh
@@ -0,0 +1,12 @@
+rm preimages.rlp
+sleep 1
+wget https://s3-us-west-2.amazonaws.com/download.energi.software/releases/chaindata/mainnet/preimages.rlp
+sudo systemctl stop energi3
+sleep 1
+energi3 import-preimages preimages.rlp
+sleep 1
+cd
+sleep 1
+curl -s https://s3-us-west-2.amazonaws.com/download.energi.software/releases/chaindata/mainnet/gen3-chaindata.tar.gz | tar xvz
+sleep 1
+sudo systemctl start energi3
