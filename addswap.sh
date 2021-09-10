eeba005fee97937d6f7199f3f3be28d82d5ae197
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-09-10 07:41:48 +0200

    Update addswap.sh

--- addswap.sh
+++ addswap.sh
@@ -3,8 +3,15 @@
 # https://www.louloucrypto.fr
  echo -e "${RED}How many running Go of swap do you want to add on this server ? ( add M after the number)"
  read -e SWAP
-SWAPFILE=$(mktemp)
-sudo dd if=/dev/zero of=$SWAPFILE bs=1024 count=$SWAP
-sudo chmod 600 $SWAPFILE
-sudo mkswap $SWAPFILE
-sudo swapon -a $SWAPFILE
+sudo fallocate -l $SWAP /swap2G.file
+sleep 1
+sudo chmod 600 /swap2G.file
+sleep 1
+sudo mkswap /swap2G.file
+sleep 1
+sudo swapon /swap2G.file
+sleep 1 
+echo '/swap2G.file none swap sw 0 0' | sudo tee -a /etc/fstab
+sleep 1
+free -h
+
