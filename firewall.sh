efe905accb4ffb51b8c46d04b9fe1b45b558f9c0
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2021-11-23 06:52:52 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -3,8 +3,8 @@
 # https://www.louloucrypto.fr
 sudo ufw allow 80
 sudo ufw allow 443
-sudo ufw allow 12124:12128/tcp
-sudo ufw allow 12124:12128/udp
+sudo ufw allow 16124:16128/tcp
+sudo ufw allow 16124:16128/udp
 sudo ufw allow 30000:39999/tcp
 sudo ufw allow 30000:39999/udp
 sudo ufw default allow outgoing
