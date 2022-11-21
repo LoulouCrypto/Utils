2b45e4bdcdc5c8ce6dbe009fd6ff226e795c72ea
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2022-11-21 19:23:26 +0100

    Update conso.sh

--- conso.sh
+++ conso.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+# Script done by LoulouCrypto
+# https://www.louloucrypto.fr
+echo -e "Lunching Radeon Power Optimisations"
+sleep 1
+motd | grep Radeon | awk -F "[ ]"  '{print "echo 0 > /sys/bus/pci/devices/0000:"$3 "/pp_dpm_mclk"  }'| bash;
+sleep 1 
+echo -e "Radeon Power Optimisations Done"
