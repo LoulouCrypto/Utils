bd3760c2fea1cf3f390a3a326be6a9756b5c3042
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2022-11-21 20:33:36 +0100

    Update conso.sh

--- conso.sh
+++ conso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script done by LoulouCrypto
 # https://www.louloucrypto.fr
-echo -e "Lunching Radeon Power Optimisations at startup"
+echo -e "Launching Radeon Power Optimisations at startup"
 
 cat >/etc/systemd/system/amdconsotweak.service<<'EOF'
 [Unit]
