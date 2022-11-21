06b59f46165aa1ff531ebd00fb445dd765f8cd84
LoulouCrypto <68774649+LoulouCrypto@users.noreply.github.com>
2022-11-21 20:26:25 +0100

    Update conso.sh

--- conso.sh
+++ conso.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 # Script done by LoulouCrypto
 # https://www.louloucrypto.fr
-echo -e "Lunching Radeon Power Optimisations"
+echo -e "Lunching Radeon Power Optimisations at startup"
+
 sleep 1
-motd | grep Radeon | awk -F "[ ]"  '{print "echo 0 > /sys/bus/pci/devices/0000:"$3 "/pp_dpm_mclk"  }'| bash;
+
+cat >/usr/local/bin/conso.sh<<'EOF'
+/hive/bin/motd | grep Radeon | awk -F "[ ]"  '{print "echo 0 > /sys/bus/pci/devices/0000:"$3 "/pp_dpm_mclk"  }'| bash;
+EOF
+
+sleep 1
+
+chmod +x /usr/local/bin/conso.sh
+
 sleep 1 
+
+cat >/etc/systemd/system/amdconsotweak.service<<'EOF'
+[Unit]
+Description=Start amdconsotweak on boot
+StartLimitIntervalSec=0
+
+[Service]
+Type=idle
+ExecStart=/usr/local/bin/conso.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sleep 1 
+systemctl daemon-reload
+sleep 1
+systemctl enable amdconsotweak.service
+sleep 1
+systemctl start amdconsotweak.service
+sleep 1
+
 echo -e "Radeon Power Optimisations Done"
